77287c003606256c3b7ce967ee4d610b15edb97a
Kinga Janicka <kingajanicka7@gmail.com>
2020-01-28 22:13:02 +0000

    Made test image randomised

--- chonky.cat/src/testImage/index.js
+++ chonky.cat/src/testImage/index.js
@@ -3,16 +3,21 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardActionArea from "@material-ui/core/CardActionArea";
 
-const testImage = ({ classes }) => (
-  <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        className={classes.media}
-        image="https://placeimg.com/345/345/animals"
-        title="animal"
-      ></CardMedia>
-    </CardActionArea>
-  </Card>
-);
+const testImage = ({ classes }) => {
+  const w = Math.round(Math.random() * 800);
+  const h = Math.round(Math.random() * 800);
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={`https://placeimg.com/${w}/${h}/animals`}
+          title="animal"
+        ></CardMedia>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 export default testImage;
