c34218ecf4a47ba33084988799f82fdf34a6fe2e
Kinga Janicka <kingajanicka7@gmail.com>
2020-02-06 23:12:14 +0000

    aa

--- api/images/[sort].js
+++ api/images/[sort].js
@@ -1,19 +1,15 @@
 import Parser from "rss-parser";
+import Snoowrap from "snoowrap";
 
 export default async (req, res) => {
   const { sort = "hot" } = req.query;
-  const endPoint = `https://www.reddit.com/r/airplaneears/.rss?sort=${sort}`;
-  const parser = new Parser();
-  const feed = await parser.parseURL(endPoint);
-  const images = feed.items
-    .map(d => ({
-      title: d.title,
-      link: d.link,
-      content: d.content.match(/https?:\/\/(?:www\.)?imgur\.com\/\w+/),
-      author: d.author,
-      isoDate: d.isoDate
-    }))
-    .filter(d => d.content);
-
-  return res.json(images);
+  console.log(process.env);
+  const r = new Snoowrap({
+    userAgent: "Chonky.cat v1",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USERNAME,
+    password: process.env.REDDIT_PASSWORD
+  });
+  return res.json(r.getSubmission("2np694"));
 };
