16f13f3b64cec4d73116795172cd2c8973fd1f6a
Kinga Janicka <kingajanicka7@gmail.com>
2021-11-13 11:06:26 +0000

    test changes to show how git works

--- src/i18n.js
+++ src/i18n.js
@@ -12,8 +12,8 @@ const resources = {
       Rising: "Rising",
       "Sort by": "Sort by",
       Back: "Back",
-      Next: "Next"
-    }
+      Next: "Next",
+    },
   },
   cat: {
     translation: {
@@ -23,9 +23,9 @@ const resources = {
       Rising: "Aixecar-se",
       "Sort by": "Ordenar per",
       Back: "Esquena",
-      Next: "Pròxim"
-    }
-  }
+      Next: "Pròxim",
+    },
+  },
 };
 
 i18n
@@ -33,12 +33,12 @@ i18n
   .init({
     resources,
     lng: "en",
-
+    ////test
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
